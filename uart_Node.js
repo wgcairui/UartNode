83ad5fb8dd5d3e64348d38f8d56e129d3ee87a89
wgcairui <wgcairui@icloud.com>
2019-12-07 10:19:50 +0800

    a

--- uart_Node.js
+++ uart_Node.js
@@ -7,39 +7,31 @@ const app = websockify(new Koa());
 // Regular middleware
 // Note it's app.ws.use and not app.use
 app.ws.use(function(ctx, next) {
-  console.log(ctx);
+ctx.websocket.send("success");
+	console.log("log is")
   // return `next` to pass the context (ctx) on to the next ws middleware
   return next(ctx);
 });
  
 // Using routes
-app.ws.use(route.all('/', function (ctx) {
+app.ws.use(route.all("/", function (ctx) {
+	ctx.websocket.send("successxsacdcdddddd");
   // `ctx` is the regular koa context created from the `ws` onConnection `socket.upgradeReq` object.
   // the websocket is added to the context on `ctx.websocket`.
   console.log(ctx);
   
-  ctx.websocket.send('Hello World');
-  ctx.websocket.
-  ctx.websocket.on('message', function(message) {
-    // do something with the message from client
-        console.log(message);
-  });
-}));
-// Using routes
-app.ws.use(route.all('/test/:id', function (ctx) {
-  // `ctx` is the regular koa context created from the `ws` onConnection `socket.upgradeReq` object.
-  // the websocket is added to the context on `ctx.websocket`.
-  console.log(ctx);
-  
-  ctx.websocket.send('Hello World');
-  ctx.websocket.
-  ctx.websocket.on('message', function(message) {
-    // do something with the message from client
-        console.log(message);
-  });
-}));
+
  
-app.listen(9000,()=>{
-    console.log(`Uart node listen port:9000`);
+  ctx.websocket.on("message", function(message) {
+    // do something with the message from client
+	console.log(message)
+	  ctx.websocket.send('axxxxxxxxxxxxxxxxxxxxx')
+	  console.log(ctx);
+  });
+
+}));
+const port = 81
+app.listen(port,()=>{
+    console.log(`Uart node listen port:${port}`);
     
 });
