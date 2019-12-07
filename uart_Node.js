4d542c288c3baaa5b5a0c95ef9ebc7990b490445
wgcairui <wgcairui@icloud.com>
2019-12-07 15:40:04 +0800

    add

--- uart_Node.js
+++ uart_Node.js
@@ -1,18 +1,23 @@
-const TcpServer = require("./lib/TcpServer")
+const TcpServer = require("./lib/TcpServer");
 
-const tcpServer = new TcpServer(9000)
+const tcpServer = new TcpServer(9000);
 
-tcpServer.on('connect', client => {
-  console.log('%s:%s connect.', client['ip'], client['port']);
+tcpServer.on("connect", client => {
+  console.log("%s:%s connect.", client["ip"], client["port"]);
 });
-tcpServer.on('data', client => {
-  let data = client['data'].toString();
-  console.log('%s:%s send: %s.', client['ip'], client['port'], data);
+
+tcpServer.on("register", client => {
+  console.log(`设备注册:Mac=${client["mac"]},Jw=${client["jw"]}`);
+  client["socket"].write("register success");
+});
+tcpServer.on("data", client => {
+  let data = client["data"].toString();
+  console.log("%s:%s send: %s.", client["ip"], client["port"], data);
   console.log(client);
-  
+
   tcpServer.broadcast(data);
 });
-tcpServer.on('close', client => {
-  console.log('%s:%s close.', client['ip'], client['port']);
+tcpServer.on("close", client => {
+  console.log("%s:%s close.", client["ip"], client["port"]);
 });
 tcpServer.start();
