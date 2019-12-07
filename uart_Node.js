d97bd4684e613ed5f608f95fe97871cca6d40694
wgcairui <wgcairui@icloud.com>
2019-12-07 14:34:23 +0800

    add

--- uart_Node.js
+++ uart_Node.js
@@ -2,13 +2,14 @@ const TcpServer = require("./lib/TcpServer")
 
 const tcpServer = new TcpServer(9000)
 
-
 tcpServer.on('connect', client => {
   console.log('%s:%s connect.', client['ip'], client['port']);
 });
 tcpServer.on('data', client => {
   let data = client['data'].toString();
   console.log('%s:%s send: %s.', client['ip'], client['port'], data);
+  console.log(client);
+  
   tcpServer.broadcast(data);
 });
 tcpServer.on('close', client => {
