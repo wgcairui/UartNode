797bceedb52aec7b27676f919dba1ede5746e6a6
wgcairui <wgcairui@icloud.com>
2019-12-07 08:32:27 +0800

    add

---
+++ uart_Node.js
@@ -0,0 +1,31 @@
+const Koa = require('koa')
+const  route = require('koa-route')
+const websockify = require('koa-websocket');
+ 
+const app = websockify(new Koa());
+ 
+// Regular middleware
+// Note it's app.ws.use and not app.use
+app.ws.use(function(ctx, next) {
+  // return `next` to pass the context (ctx) on to the next ws middleware
+  return next(ctx);
+});
+ 
+// Using routes
+app.ws.use(route.all('/test/:id', function (ctx) {
+  // `ctx` is the regular koa context created from the `ws` onConnection `socket.upgradeReq` object.
+  // the websocket is added to the context on `ctx.websocket`.
+  console.log(ctx);
+  
+  ctx.websocket.send('Hello World');
+  ctx.websocket.
+  ctx.websocket.on('message', function(message) {
+    // do something with the message from client
+        console.log(message);
+  });
+}));
+ 
+app.listen(9000,()=>{
+    console.log(`Uart node listen port:9000`);
+    
+});
