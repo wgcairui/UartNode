61048e2993d2d8fc3bbf2b940d4decb9235354bf
wgcairui <wgcairui@icloud.com>
2019-12-20 17:02:21 +0800

    完善透传流程

--- test.js
+++
@@ -1,6 +0,0 @@
-const io = require("socket.io-client")("http://localhost:3000/Node");
-io.on("connect", () => {
-  io.emit("test", { a: 2132 });
-  console.log("success");
-});
-io.on("as", data => console.log(data));
--- uart_Node.js
+++ uart_Node.js
@@ -1,31 +1,6 @@
-const TcpServer = require("./lib/TcpServer");
 const { Socket } = require("./lib/socket.client");
 
-const tcpServer = new TcpServer(9000);
-const socket = new Socket(tcpServer);
+const socket = new Socket();
 //socket注册成功后定时发送数据给server  
 socket.start()
-//监听tcp连接
-tcpServer
-  .on("connect", client => {
-    console.log("%s:%s connect.", client["ip"], client["port"]);
-  })
-  .on("close", client => {
-    console.log("%s:%s close.", client["ip"], client["port"]);
-  })
-  .on("register", client => {
-    console.log(`设备注册:Mac=${client["mac"]},Jw=${client["jw"]}`);
-    setInterval(() => {
-      console.log(`start send ups`);
-      tcpServer
-        .SendClientBind({
-          mac: client.mac,
-          type: 485,
-          content: "000300000002C5DA"
-        })
-        .then(res => console.log({ res }))
-        .catch(e => console.log({ e }));
-    }, 1000);
-  });
 
-tcpServer.start();
